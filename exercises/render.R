b4ae1901f5418b07fc6dfa2623a159d3a1416abd
Hanne Oberman <h.i.oberman@uu.nl>
2025-11-20 15:26:50 +0100

    rerender Quarto exercises

--- exercises/render.R
+++ exercises/render.R
@@ -1,19 +1,17 @@
 ### Title:    Render Exercises
 ### Author:   Kyle M. Lang
 ### Created:  2021-10-14
-### Modified: 2021-10-20
+### Modified: 2025-11-20
 
 ## Extract command line arguments:
 args <- commandArgs(trailingOnly = TRUE)
 
 ## Define an appropriate file name:
-outFile <- paste0(args[1],
-                  ifelse(as.logical(args[2]), "_solutions", ""),
-                  ".html")
+outFile <- paste0(args[1], ifelse(as.logical(args[2]), "_solutions", ""), ".html")
 
 ## Render the document:
-rmarkdown::render(input = paste0(args[1], ".Rmd"),
-                  params = list(answers = as.logical(args[2])),
-                  output_file = outFile,
-                  envir = new.env()
-                  )
+quarto::quarto_render(
+  input = paste0(args[1], ".qmd"),
+  execute_params = list(answers = as.logical(args[2])),
+  output_file = outFile
+)
